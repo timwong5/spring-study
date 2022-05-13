b5601188826509685e05a961e46172230af8d2f2
Yining Wang <wangyining203@gmail.com>
2022-05-13 20:33:45 +0800

    add the spring5 study

---
+++ spring02-hello/src/main/java/com/timwang5/pojo/hello.java
@@ -0,0 +1,21 @@
+package com.timwang5.pojo;
+
+public class hello {
+
+    private String str;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    @Override
+    public String toString() {
+        return "hello{" +
+                "str='" + str + '\'' +
+                '}';
+    }
+}
