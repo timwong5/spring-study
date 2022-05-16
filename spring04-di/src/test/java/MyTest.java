8faadeddf64221f54f58f27ea2b8e3fbe2cb6223
Yining Wang <wangyining203@gmail.com>
2022-05-16 17:31:42 +0800

    add the spring05-autowired

--- spring04-di/src/test/java/MyTest.java
+++ spring04-di/src/test/java/MyTest.java
@@ -1,4 +1,6 @@
 import com.timwang5.pojo.Students;
+import com.timwang5.pojo.User;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,5 +14,28 @@ public class MyTest {
         //System.out.println(student.getName());
 
         System.out.println(student.toString());
+    }
+
+
+    @Test
+    public void test1(){
+
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("userBean.xml");
+
+        User user = (User) context.getBean("user");
+
+        System.out.println(user.getName());
+
+    }
+
+
+    @Test
+    public void test2(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("userBean.xml");
+
+        User user2 = (User) context.getBean("user2");
+
+        System.out.println(user2.getName());
     }
 }
---
+++ spring05-autowired/src/main/java/com/timwang5/pojo/Cat.java
@@ -0,0 +1,8 @@
+package com.timwang5.pojo;
+
+public class Cat {
+
+    public void shout(){
+        System.out.println("mmmmm~");
+    }
+}
