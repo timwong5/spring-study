4e9a560dec4c39ea18ecb48419464bcb7328f9ec
Yining Wang <wangyining203@gmail.com>
2022-05-16 23:10:00 +0800

    add the spring05-autowired

---
+++ spring06-annotation/src/main/java/com/timwang5/pojo/User.java
@@ -0,0 +1,11 @@
+package com.timwang5.pojo;
+
+
+import org.springframework.stereotype.Component;
+
+//µÈ¼ÛÓÚ <bean id="user" class="com.timwang5.pojo.User"/>
+@Component
+public class User {
+
+    public String name = "tim";
+}
---
+++ spring06-annotation/src/test/java/MyTest.java
@@ -0,0 +1,17 @@
+import com.timwang5.pojo.User;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+
+
+    @Test
+    public void testUser(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        User user = context.getBean("user", User.class);
+
+        System.out.println(user.name);
+    }
+}
