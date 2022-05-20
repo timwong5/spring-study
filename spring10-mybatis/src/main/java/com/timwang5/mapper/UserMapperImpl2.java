125f1fd01336d3d9956effe59e20f2e6247c0d8f
Yining Wang <wangyining203@gmail.com>
2022-05-21 07:48:39 +0800

    finish spring10

---
+++ spring10-mybatis/src/main/java/com/timwang5/mapper/UserMapperImpl2.java
@@ -0,0 +1,20 @@
+package com.timwang5.mapper;
+
+import com.timwang5.pojo.User;
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+import java.util.List;
+
+public class UserMapperImpl2 extends SqlSessionDaoSupport implements UserMapper{
+    @Override
+    public List<User> selectUser() {
+
+        SqlSession sqlSession = getSqlSession();
+//        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+//        return mapper.selectUser();
+
+        return getSqlSession().getMapper(UserMapper.class).selectUser();
+
+    }
+}
